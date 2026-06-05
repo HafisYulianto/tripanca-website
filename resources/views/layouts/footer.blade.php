c63a845c9694b120f1692ec1dd0ad8548dde6857
HafisYulianto <hafis_yulianto@teknokrat.ac.id>
2026-06-06 00:43:03 +0700

    style: hapus formulir berlangganan email kedua di footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -20,10 +20,6 @@
                         <img src="/build/assets/img/logo.png" alt="Logo" style="width:150px; height:auto;">
                         <p class="mb-3">PT Tirta Panca Anugrah (Tripanca) adalah produsen air minum dalam kemasan terpercaya dengan komitmen menyediakan air mineral berkualitas dari sumber mata air pegunungan terpilih.</p>
                     </div>
-                    <div class="position-relative">
-                        <input class="form-control rounded-pill w-100 py-3 ps-4 pe-5" type="text" placeholder="Masukkan email Anda">
-                        <button type="button" class="btn btn-secondary rounded-pill position-absolute top-0 end-0 py-2 mt-2 me-2">Daftar</button>
-                    </div>
                 </div>
             </div>
             <div class="col-md-6 col-lg-6 col-xl-3">
